71cf0c2685b3a5df1ec94be17e43a958c1c18d49
Dwcks Dinh <corclan19@gmail.com>
2024-03-20 12:23:24 +0700

    21 can u do some for me

---
+++ shopee-clone/helpers.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Get the base path
+ * 
+ * @param string $path
+ * @return string
+ */
+function basePath($path = '') {
+    return __DIR__ . "/" . $path;
+}
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView($name) {
+    $viewPath = basePath("");
+}
